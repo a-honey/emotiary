4d3e97083ea3012ee097b9ee6fd96e3b3b47e67f
a-honey <jungahyeon0512@khu.ac.kr>
2023-10-04 15:43:00 +0900

    Feat: 작성 컴포넌트 추가

--- front/src/pages/MainPage.jsx
+++ front/src/pages/MainPage.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import Calendar from '../components/common/calendar/Calendar';
+import DiaryWriting from '../components/main/DiaryWriting';
 
 const MainPage = () => {
+  const [isDiaryWriting, setIsDiaryWriting] = useState(false);
+
+  const handleIsDiaryWriting = (boolean) => {
+    setIsDiaryWriting(boolean);
+  };
+
   return (
     <main>
       <Calendar />
+      <DiaryWriting handleIsDiaryWriting={handleIsDiaryWriting} />
     </main>
   );
 };
