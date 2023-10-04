40407a379e11ebccf22e7b11315ec7d5de292b23
a-honey <jungahyeon0512@khu.ac.kr>
2023-10-04 17:59:45 +0900

    네트워크페이지 생성 및 라우팅

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import IntroPage from './pages/IntroPage';
+import NetworkPage from './pages/UsersPage';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/intro" exact element={<IntroPage />} />
       <Route path="/" exact element={<MainPage />} />
+      <Route path="/network" exact element={<NetworkPage />} />
       <Route path="*" element={<MainPage />} />
     </Routes>
   );
---
+++ front/src/pages/UsersPage.jsx
@@ -0,0 +1,11 @@
+import DiaryList from '../components/network/DiaryList';
+
+const NetworkPage = () => {
+  return (
+    <main>
+      <DiaryList />
+    </main>
+  );
+};
+
+export default NetworkPage;
