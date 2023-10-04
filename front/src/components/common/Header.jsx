7ee245fa7b6ddf11fdef6a41a914da9ac6e4e27e
a-honey <jungahyeon0512@khu.ac.kr>
2023-10-04 18:47:23 +0900

    상태에 따른 유저정보 렌더링

--- front/src/components/common/Header.jsx
+++ front/src/components/common/Header.jsx
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom';
 import styles from './layout.module.scss';
+import { handleImgError } from '../../utils/handleImg';
 
 const Header = () => {
+  const isLogin = false;
+
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>EMOTIARY</div>
@@ -11,10 +14,17 @@ const Header = () => {
         <Link to="/users">ALL USERS</Link>
         <Link to="/analysis">ANALYSIS</Link>
       </nav>
-      <div>
-        <Link to="/login">LOGIN</Link>
-        <Link to="/register">REGISTER</Link>
-      </div>
+      {isLogin ? (
+        <div className={styles.userInfo}>
+          <img src="" alt="의 프로필사진" onError={handleImgError} />
+          <div>유저이름</div>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">LOGIN</Link>
+          <Link to="/register">REGISTER</Link>
+        </div>
+      )}
     </header>
   );
 };
--- front/src/hooks/useIsLogin.js
+++ front/src/hooks/useIsLogin.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
-import { userNameSelector } from '../components/state/selectors/userSelector';
+import { userNameSelector } from '../components/states/selectors/userSelector';
 
 // 로그인 상태가 아닐 경우 intro 페이지 이동, 로그인 상태일 경우 true를 반환하는 hook
 const useIsLogin = () => {
@@ -11,13 +11,19 @@ const useIsLogin = () => {
   // localStorage에서 token을 가져옴
   const token = localStorage.getItem('userToken');
 
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   useEffect(() => {
     if (!token || userName === '') {
       // token이 없거나, userName이 없는 경우(비 로그인상태) /intro로 이동
+      if (['/intro', '/login', '/register'].includes(currentPath)) {
+        return false;
+      }
       navigator('/intro'); // 로그인 상태가 아님을 반환
       return false;
     }
-  }, [navigator, token, userName]);
+  }, [navigator, token, userName, currentPath]);
 
   return true; // 로그인 상태임을 반환
 };
