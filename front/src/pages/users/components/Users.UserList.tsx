202f164012d9fe2deb759e0ef7e114f5e46b564b
a-honey <jungahyeon0512@khu.ac.kr>
2023-10-10 04:00:21 +0900

    Chore: fakedata useremail 제거

--- front/src/pages/users/components/Users.UserList.tsx
+++ front/src/pages/users/components/Users.UserList.tsx
@@ -6,7 +6,6 @@ import { useGetUsersData } from '../../../api/get/useGetUserData';
 
 interface UserItemType {
   user_id: number;
-  useremail: string;
   username: string;
   description: string;
   profileImage: string;
@@ -16,56 +15,48 @@ const UserList = () => {
   const fakeData = [
     {
       user_id: 1,
-      useremail: 'user1@example.com',
       username: 'User 1',
       description: 'Description for User 1',
       profileImage: 'profile1.jpg',
     },
     {
       user_id: 2,
-      useremail: 'user2@example.com',
       username: 'User 2',
       description: 'Description for User 2',
       profileImage: 'profile2.jpg',
     },
     {
       user_id: 3,
-      useremail: 'user3@example.com',
       username: 'User 3',
       description: 'Description for User 3',
       profileImage: 'profile3.jpg',
     },
     {
       user_id: 4,
-      useremail: 'user4@example.com',
       username: 'User 4',
       description: 'Description for User 4',
       profileImage: 'profile4.jpg',
     },
     {
       user_id: 5,
-      useremail: 'user5@example.com',
       username: 'User 5',
       description: 'Description for User 5',
       profileImage: 'profile5.jpg',
     },
     {
       user_id: 6,
-      useremail: 'user6@example.com',
       username: 'User 6',
       description: 'Description for User 6',
       profileImage: 'profile6.jpg',
     },
     {
       user_id: 7,
-      useremail: 'user7@example.com',
       username: 'User 7',
       description: 'Description for User 7',
       profileImage: 'profile7.jpg',
     },
     {
       user_id: 8,
-      useremail: 'user8@example.com',
       username: 'User 8',
       description: 'Description for User 8',
       profileImage: 'profile8.jpg',
