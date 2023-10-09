c3f74aab63422785bb670511e20bac607b0fc492
a-honey <jungahyeon0512@khu.ac.kr>
2023-10-10 04:25:53 +0900

    Feat: staleTime, cachTime Infinity 설정

--- front/src/index.js
+++ front/src/index.js
@@ -8,7 +8,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity, // 데이터가 유효한 최대 시간
+      cacheTime: Infinity, // 캐시된 데이터가 보존되는 최대 시간
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
