a92c4dfbad60557574c6e1f937c9a5b6c5ff64a5
mixsam <mixsam.taurusium@gmail.com>
2023-10-11 11:44:39 +0900

    Fix:로그인과 등록의 백엔드 엔드포인트 설정

--- front/src/components/signup/Signup.tsx
+++ front/src/components/signup/Signup.tsx
@@ -1,4 +1,6 @@
+
 import React, { FormEvent, useState } from 'react';
+import axios from 'axios';
 import '../../styles/Signup.css';
 
 const Signup: React.FC = () => {
@@ -6,7 +8,7 @@ const Signup: React.FC = () => {
   const [password, setPassword] = useState<string>('');
   const [confirmPassword, setConfirmPassword] = useState<string>('');
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
 
     if (password !== confirmPassword) {
@@ -14,7 +16,19 @@ const Signup: React.FC = () => {
       return;
     }
 
-    console.log('회원가입 정보', { email, password });
+    const data = { email, password, confirmPassword };
+
+    try {
+      const response = await axios.post('http://localhost:3000/users/register', data, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log('회원가입 성공', response.data);
+    } catch (error) {
+      console.log('회원가입 실패', error);
+    }
+
   };
 
   return (
@@ -68,3 +82,91 @@ const Signup: React.FC = () => {
 }
 
 export default Signup;
+
+
+
+// import React, { FormEvent, useState } from 'react';
+// import { useMutation, useQueryClient } from 'react-query';
+// import axios from 'axios';
+// import '../../styles/Signup.css';
+
+// // 회원가입 API 호출
+// const signupUser = async ({ email, password }: { email: string; password: string }) => {
+//   const response = await axios.post('http://example.com/api/signup', { email, password });
+//   return response.data;
+// };
+
+// const Signup: React.FC = () => {
+//   const [email, setEmail] = useState<string>('');
+//   const [password, setPassword] = useState<string>('');
+//   const [confirmPassword, setConfirmPassword] = useState<string>('');
+//   const queryClient = useQueryClient();
+  
+//   const mutation = useMutation(signupUser, {
+//     onSuccess: () => {
+//       queryClient.invalidateQueries('someQueryKey'); // 성공시 캐시 무효화
+//     },
+//   });
+
+//   const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+//     e.preventDefault();
+
+//     if (password !== confirmPassword) {
+//       alert('패스워드가 일치하지 않습니다.');
+//       return;
+//     }
+
+//     mutation.mutate({ email, password });
+//   };
+
+//   return (
+//     <>
+//       <div className='centerContainer'>
+//         <form onSubmit={handleSubmit} className='signupForm'>
+//           <div className='formGroup'>
+//             <label htmlFor="email"></label>
+//             <div className='inputGroup'>
+//               <i className='box1'></i>
+//               <input
+//                 id="email"
+//                 type="email"
+//                 placeholder="이메일을 입력하세요"
+//                 value={email}
+//                 onChange={(e) => setEmail(e.target.value)}
+//               />
+//             </div>
+//           </div>
+//           <div className='formGroup'>
+//             <label htmlFor="password"></label>
+//             <div className='inputGroup'>
+//               <i className='box2'></i>
+//               <input
+//                 id="password"
+//                 type="password"
+//                 placeholder="패스워드를 입력하세요"
+//                 value={password}
+//                 onChange={(e) => setPassword(e.target.value)}
+//               />
+//             </div>
+//           </div>
+//           <div className='formGroup'>
+//             <label htmlFor="confirmPassword"></label>
+//             <div className='inputGroup'>
+//               <i className='box2'></i>
+//               <input
+//                 id="confirmPassword"
+//                 type="password"
+//                 placeholder="패스워드를 다시 입력하세요"
+//                 value={confirmPassword}
+//                 onChange={(e) => setConfirmPassword(e.target.value)}
+//               />
+//             </div>
+//           </div>
+//           <button type="submit" className='submitButton'>SIGN UP</button>
+//         </form>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Signup;
