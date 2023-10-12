e73e7d5b94beabd678646b5acc8577d3daf5102b
[AI8/1반] 박지호 <wlghwk0509@gmail.com>
2023-10-12 21:26:04 +0900

    Refactor: 댓글 조회 관련 수정

--- back/src/services/commentService.ts
+++ back/src/services/commentService.ts
@@ -3,101 +3,107 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function createdComment(
-    inputData: {
-      content: string,
-      nestedComment: string,
-    },
-    authorId: string,
-    diary_id: string,
-    ){
-      try {
-          const { content, nestedComment } = inputData 
-          const comment = await prisma.comment.create({
-              data: { diaryId: diary_id, authorId, content, nestedComment }
-          });
-          return comment;
-      } catch(error) {
-          throw error;
-      }
-    }
+  inputData: {
+    content: string;
+    nestedComment: string;
+  },
+  authorId: string,
+  diary_id: string,
+) {
+  try {
+    const { content, nestedComment } = inputData;
+    const comment = await prisma.comment.create({
+      data: { diaryId: diary_id, authorId, content, nestedComment },
+    });
+    return comment;
+  } catch (error) {
+    throw error;
+  }
+}
 
-export async function getCommentByDiaryId(
-    diary_id: string,
-    ){
-      try {
-          const comment = await prisma.comment.findMany({
-              where: { diaryId: diary_id },
+export async function getCommentByDiaryId(diary_id: string) {
+  try {
+    const comment = await prisma.comment.findMany({
+      where: { diaryId: diary_id, nestedComment: null },
+      select: {
+        id: true,
+        author: {
+          select: {
+            id: true,
+            username: true,
+            profileImage: true,
+          },
+        },
+        diaryId: true,
+        content: true,
+        createdAt: true,
+        updatedAt: true,
+        reComment: {
+          select: {
+            id: true,
+            author: {
               select: {
-                  id: true,
-                  author: {
-                      select: {
-                          id: true,
-                          username: true,
-                          profileImage: true,
-                      }
-                  },
-                  diaryId: true,
-                  content: true,
-                  nestedComment: true,
-                  createdAt: true,
-                  updatedAt: true,
+                id: true,
+                username: true,
+                profileImage: true,
               },
-              orderBy: { createdAt: 'asc'}
-          });
-          return { data: comment };
-      } catch(error) {
-          throw error;
-      }
-    }
+            },
+            diaryId: true,
+            content: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+      },
+      orderBy: { createdAt: 'asc' },
+    });
+    return { data: comment };
+  } catch (error) {
+    throw error;
+  }
+}
 
 export async function updatedComment(
   inputData: {
-    content: string,
+    content: string;
   },
   comment_id: string,
   authorId: string,
-  ){
-    try {
-        const userCheck = await prisma.comment.findUnique({
-            where: { id: comment_id }
-        })
+) {
+  try {
+    const userCheck = await prisma.comment.findUnique({
+      where: { id: comment_id },
+    });
 
-        if (userCheck.authorId == authorId) {
-            const comment = await prisma.comment.update({
-                where: { id: comment_id },
-                data: inputData
-            });
-            return { data: comment };
-        } else {
-            return { message: '작성자가 아니므로 수정이 불가합니다.' }
-        }
-
-    } catch(error) {
-        throw error;
+    if (userCheck.authorId == authorId) {
+      const comment = await prisma.comment.update({
+        where: { id: comment_id },
+        data: inputData,
+      });
+      return { data: comment };
+    } else {
+      return { message: '작성자가 아니므로 수정이 불가합니다.' };
     }
+  } catch (error) {
+    throw error;
   }
+}
 
-export async function deletedComment(
-  comment_id: string,
-  authorId: string,
-  ){
-    try {
-        const userCheck = await prisma.comment.findUnique({
-          where: { id: comment_id }
-        })
-        if (userCheck.authorId == authorId) {
-            await prisma.comment.deleteMany({
-                where: { id: comment_id }
-            });
-            await prisma.comment.deleteMany({
-                where: { nestedComment: comment_id }
-            });
-            return { message: '해당 댓글을 삭제했어요!' }
-      } else {
-          return { message: '작성자가 아니므로 삭제가 불가합니다.' }
-      }
+export async function deletedComment(comment_id: string, authorId: string) {
+  try {
+    const userCheck = await prisma.comment.findUnique({
+      where: { id: comment_id },
+    });
+    if (userCheck.authorId == authorId) {
+      await prisma.comment.deleteMany({
+        where: { id: comment_id },
+      });
 
-    } catch(error) {
-        throw error;
+      return { message: '해당 댓글을 삭제했어요!' };
+    } else {
+      return { message: '작성자가 아니므로 삭제가 불가합니다.' };
     }
+  } catch (error) {
+    throw error;
   }
+}
