be3bbeb26490ef1d5c3f470e627933c49f259f76
leehyebin <970905hyebin@gmail.com>
2023-10-13 23:39:12 +0900

    Rename: passport-config 이동

--- back/src/passport-config/passport.ts
+++
@@ -1,6 +0,0 @@
-import jwtStrategy from "./strategy/jwt";
-import localStrategy from "./strategy/local";
-import googleStrategy from "./strategy/googleStrategy";
-
-// export { jwtStrategy, localStrategy };
-export { jwtStrategy, localStrategy, googleStrategy };
