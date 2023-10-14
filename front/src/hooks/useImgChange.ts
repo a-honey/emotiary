7aa518dd9784e82bc7b686ad87f09d5c0580c813
a-honey <jungahyeon0512@khu.ac.kr>
2023-10-14 17:03:36 +0900

    Feat: 파일 업로드 dnd

--- front/src/hooks/useImgChange.ts
+++ front/src/hooks/useImgChange.ts
@@ -1,13 +1,29 @@
-import { useState, useRef, ChangeEvent } from 'react';
+import { useState, useRef, ChangeEvent, DragEvent } from 'react';
 
 const useImgChange = () => {
   const [imgContainer, setImgContainer] = useState<File | null>(null);
   // src를 변경할 요소(useRef 객체)
   const imgRef = useRef<HTMLImageElement | null>(null);
 
-  const handleImgChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const img = e.target.files?.[0];
+  const handleImgChange = (
+    e: ChangeEvent<HTMLInputElement> | DragEvent<HTMLInputElement>,
+  ) => {
+    let img: File | null = null;
 
+    if (
+      'dataTransfer' in e &&
+      e.dataTransfer &&
+      e.dataTransfer.files &&
+      e.dataTransfer.files.length > 0
+    ) {
+      // 드래그 앤 드롭 이벤트에서 파일 가져오기
+      if (e.dataTransfer && e.dataTransfer.files.length > 0) {
+        img = e.dataTransfer.files[0];
+      }
+    } else if (e.target instanceof HTMLInputElement && e.target.files) {
+      // 파일 입력 필드에서 파일 가져오기
+      img = e.target.files[0];
+    }
     if (imgRef.current && !img) {
       imgRef.current.src = '';
       return;
