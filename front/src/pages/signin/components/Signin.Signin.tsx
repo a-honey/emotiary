56d6c1b2e22a8ce53274296788e7c263b53ba7bf
mixsam <mixsam.taurusium@gmail.com>
2023-10-15 09:48:26 +0900

    Fix:로그인 구조수정

--- front/src/pages/signin/components/Signin.Signin.tsx
+++ front/src/pages/signin/components/Signin.Signin.tsx
@@ -8,24 +8,36 @@ const Signin: React.FC = () => {
   const [password, setPassword] = useState<string>('');
   const navigate = useNavigate();
 
+  const saveToLocalStorage = (data: any) => {
+    localStorage.setItem('userId', data.id);
+    localStorage.setItem('username', data.name);
+    localStorage.setItem('token', data.token);
+    localStorage.setItem('userImg', data.uploadFile);
+    localStorage.setItem('refreshToken', data.refreshToken);
+  };
+
   const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
 
     const data = { email, password };
 
     try {
-      const response = await instance.post('http://localhost:5001/users/login', data);
-      console.log('로그인 성공!', response.data);
+      const response = await instance.post(
+        'http://localhost:5001/users/login',
+        data,
+      );
+      console.log('로그인 성공!', response.data.data);
 
-      // 로컬 스토리지에 로그인 성공값을 저장
-      localStorage.setItem('userId', response.data.id);
-      localStorage.setItem('username', response.data.username);
-      localStorage.setItem('token', response.data.token);
+      saveToLocalStorage(response.data.data);
 
       // 메인 경로로 이동
       navigate('/');
     } catch (error) {
-      console.log('로그인 실패!', error);
+      if (error instanceof Error) {
+        console.log('로그인 실패!', error.message);
+      } else {
+        console.log('로그인 실패! 알 수 없는 오류:', error);
+      }
     }
   };
 
@@ -59,7 +71,9 @@ const Signin: React.FC = () => {
               />
             </div>
           </div>
-          <button type="submit" className={styles.submitButton}>SIGN IN</button>
+          <button type="submit" className={styles.submitButton}>
+            SIGN IN
+          </button>
         </form>
       </div>
     </>
@@ -67,95 +81,3 @@ const Signin: React.FC = () => {
 };
 
 export default Signin;
-
-// 비밀번호 분실
-// /users/forgot-password
-
-// 비밀번호 재설정
-// /users/reset-password
-
-// 로그아웃
-// /users/logout
-
-// 네이버 로그인
-// /users/auth/google
-
-
-// import React, { FormEvent, useState } from 'react';
-// import { useMutation } from 'react-query';
-// import '../../styles/Signin.css';
-
-// type LoginData = {
-//   email: string;
-//   password: string;
-// };
-
-// const Signin: React.FC = () => {
-//   const [email, setEmail] = useState<string>('');
-//   const [password, setPassword] = useState<string>('');
-
-//   const mutation = useMutation((newTodo: LoginData) => fetch('/api/login', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(newTodo),
-//   }));
-
-//   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-
-//     // 서버로 보낼 데이터
-//     const data = { email, password };
-
-//     // React Query를 사용해 데이터를 전송
-//     mutation.mutate(data, {
-//       onSuccess: () => {
-//         // 성공시 작업
-//         console.log("로그인 성공!");
-//       },
-//       onError: () => {
-//         // 실패시 작업
-//         console.log("로그인 실패!");
-//       },
-//     });
-//   };
-
-//   return (
-//     <>
-//       <div className='centerContainer'>
-//         <form onSubmit={handleSubmit} className='loginForm'>
-//           <div className='formGroup'>
-//             <label htmlFor="email"></label>
-//             <div className='inputGroup'>
-//               <i className='box1'></i>
-//               <input
-//                 type="email"
-//                 id="email"
-//                 placeholder="이메일을 입력하세요"
-//                 value={email}
-//                 onChange={(e) => setEmail(e.target.value)}
-//               />
-//             </div>
-//           </div>
-//           <div className='formGroup'>
-//             <label htmlFor="password"></label>
-//             <div className='inputGroup'>
-//               <i className='box2'></i>
-//               <input
-//                 type="password"
-//                 id="password"
-//                 placeholder="패스워드를 입력하세요"
-//                 value={password}
-//                 onChange={(e) => setPassword(e.target.value)}
-//               />
-//             </div>
-//           </div>
-//           <button type="submit" className='submitButton'>SIGN IN</button>
-//         </form>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Signin;
