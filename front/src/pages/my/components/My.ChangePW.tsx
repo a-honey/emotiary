27350dd2cb3f1d9d8fccdbf6e3b2d27989af06ef
a-honey <jungahyeon0512@khu.ac.kr>
2023-10-17 18:59:10 +0900

    Feat: 비밀번호 변경 모달 api 설정

--- front/src/pages/my/components/My.ChangePW.tsx
+++ front/src/pages/my/components/My.ChangePW.tsx
@@ -1,4 +1,13 @@
 import React, { useState } from 'react';
+import styles from '../styles/My.ChangePW.module.scss';
+import { getEmail } from '../../../utils/localStorageHandlers';
+import { instance } from '../../../api/instance';
+
+interface ChangeDataType {
+  email: string;
+  password: string;
+  passwordConfirm: string;
+}
 
 const CHANGE_DATA_INITIAL_DATA = {
   email: '',
@@ -13,26 +22,78 @@ const ChangePW = ({
 }) => {
   const [changeData, setChangeData] = useState(CHANGE_DATA_INITIAL_DATA);
 
-  const handleSubmit = () => {
-    //e.preventDefault();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
     if (!changeData.email) {
-      alert('이메일을 확인해주세요');
+      alert('이메일을 입력해주세요');
+      return;
+    } else if (changeData.email !== getEmail) {
+      alert('입력하신 이메일이 다릅니다.');
+      setChangeData(CHANGE_DATA_INITIAL_DATA);
+      return;
     }
+
+    if (!changeData.password) {
+      alert('비밀번호를 입력해주세요');
+      setChangeData(CHANGE_DATA_INITIAL_DATA);
+      return;
+    } else if (changeData.password !== changeData.passwordConfirm) {
+      alert('비밀번호를 다시입력해주세요');
+      setChangeData(CHANGE_DATA_INITIAL_DATA);
+      return;
+    }
+
+    try {
+      await instance.post(`/users/reset-password`, {
+        email: changeData.email,
+        password: changeData.password,
+      });
+    } catch {
+      console.error('비밀번호 변경 실패');
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setChangeData((prevData: ChangeDataType) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
   return (
     <div className="modal">
-      <form onSubmit={handleSubmit}>
+      <form className={styles.container} onSubmit={handleSubmit}>
         <label>이메일입력</label>
-        <input />
-        <label>비밀번호변경</label>
-        <input />
-        <label>비밀번호변경</label>
-        <input />
-        <div className="btns">
-          <button type="submit">변경완료</button>
-          <button type="button" onClick={toggleIsOpenChangePW}>
+        <input
+          name="email"
+          value={changeData.email}
+          onChange={handleInputChange}
+        />
+        <label>비밀번호 입력</label>
+        <input
+          type="password"
+          name="password"
+          value={changeData.password}
+          onChange={handleInputChange}
+        />
+        <label>비밀번호 재입력</label>
+        <input
+          type="password"
+          name="passwordConfirm"
+          value={changeData.passwordConfirm}
+          onChange={handleInputChange}
+        />
+        <div className={styles.btns}>
+          <button type="submit" className="doneBtn">
+            변경완료
+          </button>
+          <button
+            type="button"
+            className="cancelBtn"
+            onClick={toggleIsOpenChangePW}
+          >
             변경취소
           </button>
         </div>
