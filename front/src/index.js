0b666e68fa23c998b34107326f2cd7f1d3d67ebe
a-honey <jungahyeon0512@khu.ac.kr>
2023-10-19 03:14:11 +0900

    Feat: 쿼리 캐시로 useQuery 에러 다루기

--- front/src/index.js
+++ front/src/index.js
@@ -6,7 +6,11 @@ import './styles/global.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,6 +22,15 @@ const queryClient = new QueryClient({
       },
     },
   },
+  queryCache: new QueryCache({
+    onSuccess: () => {},
+    onSettled: () => {},
+    onError: (err, query) => {
+      if (err.meta.eMessage) {
+        console.log(`${query.queryKey} 에러`, err.meta.errorMessage);
+      }
+    },
+  }),
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
