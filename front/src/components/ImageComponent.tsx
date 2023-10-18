09173d2fee07c29b0a8f668725b942d07cc1d275
a-honey <jungahyeon0512@khu.ac.kr>
2023-10-18 12:39:18 +0900

    Feat: 다이어리 show 컴포넌트 api 연결

--- front/src/components/ImageComponent.tsx
+++ front/src/components/ImageComponent.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import user_none from '../assets/user_none.png';
-const handleImgError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
-  const imgElement = e.target as HTMLImageElement;
-
-  imgElement.src = user_none; // 이미지 오류시 대체 이미지 설정
-  imgElement.alt = '이미지 로드 중 오류 발생'; // 이미지 오류시 대체 alt 설정
-};
+import { handleImgError } from '../utils/imgHandlers';
 
 const ImageComponent = ({ src, alt }: { src: string | null; alt: string }) => {
+  return <img src={src ?? user_none} alt={alt} onError={handleImgError} />;
+};
+
+// 이미지 여러개 혹은 비디오
+export const ImagePostComponent = ({
+  src,
+  alt,
+}: {
+  src: string | null;
+  alt: string;
+}) => {
   return <img src={src ?? user_none} alt={alt} onError={handleImgError} />;
 };
 
--- front/src/components/modal/DiaryItemShow.tsx
+++ front/src/components/modal/DiaryItemShow.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
-import styles from './index.module.scss';
+import styles from './DiaryItemShow.module.scss';
 import DiaryComment from './DiaryComment';
-import DiaryReplyAdd from './DiaryReplyAdd';
+import DiaryReplyAdd from './DiaryCommentReplyAdd';
 import { useGetDiaryData } from '../../api/get/useGetDiaryData';
 import { instance } from '../../api/instance';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { GoHeartFill, GoHeart } from 'react-icons/go';
+import ImageComponent from '../ImageComponent';
+import { useNavigate } from 'react-router-dom';
+import { useGetCommentData } from '../../api/get/userGetCommendData';
 
 // 작성자 id가 로그인 id와 같을 경우, 수정 버튼 활성화, 다를경우 비활성화
 // 비공개일 경우 그냥 안띄우기, 친구공개일 경우 자물쇠 아이콘 + 친구만 볼 수 있습니다 모달 안에 띄우기
@@ -15,29 +19,23 @@ const DiaryItemShow = ({
   id: string;
   toggleIsOpenModal: () => void;
 }) => {
-  const { data, isFetching } = useGetDiaryData({ id });
+  const navigator = useNavigate();
 
-  const {
-    id: diaryId,
-    authorId,
-    createdDate,
-    title,
-    content,
-    is_public,
-    emoji,
-  } = data.data;
+  const { data: diaryData, isFetching: isDiaryDataFetching } = useGetDiaryData({
+    id,
+  });
 
   const queryClient = useQueryClient();
 
   const mutation = useMutation(
     async () => {
-      await instance.post(`/favorites/${diaryId}`);
+      await instance.post(`/favorites/${diaryData.id}`);
       return;
     },
     {
       onSuccess: () => {
         // 다이어리가 새로 생겼으므로, myAllDiarysData, myDiaryData 변경 필요
-        queryClient.invalidateQueries(['diaryData', diaryId]);
+        queryClient.invalidateQueries(['diaryData', diaryData.id]);
       },
       onError: (error) => {
         console.error('useMutation api 요청 에러', error);
@@ -48,17 +46,44 @@ const DiaryItemShow = ({
   const handleLikeClick = () => {
     mutation.mutate();
   };
+
   return (
     <div className="modal">
       <div className={styles.modalContainer}>
-        <h3>{createdDate}</h3>
-        <div>
-          <h1>{title}</h1>
-          <p>{content}</p>
-        </div>
-        <div className={styles.like} onClick={handleLikeClick}>
-          ♡
-        </div>
+        {isDiaryDataFetching ? (
+          <div>로딩중</div>
+        ) : (
+          <div className={styles.diaryContent}>
+            <div className={styles.titles}>
+              <h2>{diaryData.title}</h2>
+              <div className={styles.like} onClick={handleLikeClick}>
+                {diaryData.favoriteCount === 0 ? <GoHeart /> : <GoHeartFill />}
+                <div>{diaryData.favoriteCount}</div>
+              </div>
+            </div>
+            <div className={styles.diaryInfo}>
+              <div>
+                {diaryData.createdDate.split('T')[0].split('-')[0]}년{' '}
+                {diaryData.createdDate.split('T')[0].split('-')[1]}월{' '}
+                {diaryData.createdDate.split('T')[0].split('-')[2]}일{' '}
+              </div>
+              <div
+                className={styles.userInfo}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  navigator(`/user/${diaryData.authorId}`);
+                }}
+              >
+                <ImageComponent
+                  src={diaryData.author.profileImage ?? '/user_none.png'}
+                  alt={`${diaryData.author.username}의 프로필사진`}
+                />
+                <div>{diaryData.author.username}</div>
+              </div>
+            </div>
+            <p>{diaryData.content}</p>
+          </div>
+        )}
         <DiaryComment />
         <button className="cancelBtn" onClick={toggleIsOpenModal}>
           닫기
