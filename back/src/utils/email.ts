f7596e3401ac09084bc998d4b56f9042320e407e
[AI8/1반] 박지호 <wlghwk0509@gmail.com>
2023-10-18 17:52:02 +0900

    feat: 알림 기능 개발

--- back/src/utils/email.ts
+++ back/src/utils/email.ts
@@ -5,27 +5,33 @@ dotenv.config();
 // 환경 변수에서 이메일 서비스, 사용자, 비밀번호를 가져옵니다.
 const { email_service, user, pass } = process.env;
 
-export const sendEmail = async(to : string, subject : string, text : string) : Promise<void> => {
-    try{
-        // 이메일을 전송하기 위한 transporter(전송자) 객체를 생성합니다.
-        const transporter = nodemailer.createTransport({
-            service : email_service,
-            auth : {
-                user : user,
-                pass : pass,
-            },
-        });
+export const sendEmail = async (
+  to: string,
+  subject: string,
+  text: string,
+  html: any,
+): Promise<void> => {
+  try {
+    // 이메일을 전송하기 위한 transporter(전송자) 객체를 생성합니다.
+    const transporter = nodemailer.createTransport({
+      service: email_service,
+      auth: {
+        user: user,
+        pass: pass,
+      },
+    });
 
-        const mailOptions = {
-            from : user,
-            to,
-            subject,
-            text,
-        };
+    const mailOptions = {
+      from: user,
+      to,
+      subject,
+      text,
+      html,
+    };
 
-        // 설정한 이메일 옵션을 사용하여 이메일을 전송합니다.
-        await transporter.sendMail(mailOptions);
-    }catch(error){
-        throw error;
-    }
-}
+    // 설정한 이메일 옵션을 사용하여 이메일을 전송합니다.
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    throw error;
+  }
+};
