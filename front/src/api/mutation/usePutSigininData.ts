692d1ec9efe2d788058ac688ca058d23ef0608bd
mixsam <mixsam.taurusium@gmail.com>
2023-10-23 23:42:57 +0900

    Fix:엔드포인트 오류 수정

---
+++ front/src/api/mutation/usePutSigininData.ts
@@ -0,0 +1,33 @@
+import { QueryClient, useMutation } from '@tanstack/react-query';
+import { instance } from '../instance';
+import { useNavigate } from 'react-router-dom';
+
+export const usePutSigninData = (
+  queryClient: QueryClient,
+) => {
+  const navigate = useNavigate();
+  const signinMutation = useMutation(
+    async (userSigninInfos: { email: string; password: string }) => {
+      const response = await instance.post('/users/login', userSigninInfos);
+      return response.data;
+    },
+    {
+      onSuccess: (data: any) => {
+        console.log('로그인 성공', data.data);
+        localStorage.setItem('email', data.data.email);
+        localStorage.setItem('userId', data.data.id);
+        localStorage.setItem('username', data.data.name);
+        localStorage.setItem('token', data.data.token);
+        localStorage.setItem('userImg', data.data.uploadFile);
+        localStorage.setItem('refreshToken', data.data.refreshToken);
+
+        navigate('/');
+      },
+      onError: (error: any) => {
+        console.log('로그인 실패', error);
+      },
+    },
+  );
+
+  return signinMutation;
+};
