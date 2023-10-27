2999b5b8894de5258b8d0e97007daa753da3dbf9
lavven7 <lavven777@gmail.com>
2023-10-27 10:54:02 +0900

    Remove: 불필요한 코드와 주석 정리

--- ai/flask/views/diary_emotion_predict.py
+++ ai/flask/views/diary_emotion_predict.py
@@ -2,7 +2,6 @@ from transformers import TextClassificationPipeline, BertForSequenceClassificati
 from kobert_tokenizer import KoBERTTokenizer
 from kiwipiepy import Kiwi
 import itertools
-import json
 
 
 # 모델의 전체 파일 경로 지정
@@ -27,11 +26,6 @@ def emotion_predicts(sentence):
 
     for i in range(len(kiwi_sentenc)):
         kiwi_sentenc_list.append([kiwi_sentenc[i][0]])
-        # kiwi_sentenc_list = [item for item in kiwi_sentenc[i][0]]
-        print(f'kiwi_sentenc[{i}][0]', kiwi_sentenc[i][0], '\n\n')
-        print(f'kiwi_sentenc_list[{i}]', kiwi_sentenc_list[i], '\n\n')
-
-    print(kiwi_sentenc, '\n\n', kiwi_sentenc_list, '\n\n')
 
     # 배열이 3중으로 겹쳐있음
     sentence_predict = []
@@ -43,18 +37,11 @@ def emotion_predicts(sentence):
         sentence_predict_list.append(sentence_predict)
 
     sentence_cnt = len(sentence_predict_list)
-    print('문장의 개수: ', sentence_cnt)
-
-    # 아무 것도 안 건드린 sentence_predict_list
-    print(sentence_predict_list, '\n\n')
 
     # 3중 리스트 -> 2중 리스트
     result = list(itertools.chain(*sentence_predict_list))
-    print(result, '\n\n')
-
     # 2중 리스트 -> 1차원 리스트
     result_re = list(itertools.chain(*result))
-    print(result_re)
 
     # 문장의 각 감정 확률의 총합 구하기
     label_scores = {}
@@ -63,7 +50,6 @@ def emotion_predicts(sentence):
         label = dictionaries['label']
         score = dictionaries['score']
 
-        # Update the label score in the dictionary
         if label in label_scores:
             label_scores[label] += score
         else:
@@ -79,13 +65,13 @@ def emotion_predicts(sentence):
     sorted_labels = sorted(scores_avarage, key=lambda x: scores_avarage[x], reverse=True)
 
     # top_labels : 반환할 상위 3개의 감정
-    top_labels = [{"label": label, "average_score": scores_avarage[label]} for label in sorted_labels[:3]]
-
-    print('top_labels : ', top_labels)
-
-
-    # Convert top_labels to a JSON string
-    # json_result = json.dumps(top_labels)
-    # print('json_result : ', json_result)
+    top_labels = [
+        {
+            "label": label,
+            "average_score": scores_avarage[label] * 100,
+            "label_scores": label_scores[label] * 100
+        }
+        for label in sorted_labels[:3]
+    ]
 
     return top_labels
