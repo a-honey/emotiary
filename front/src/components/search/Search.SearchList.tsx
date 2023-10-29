631ac218f887ad82ae62bbd5dfc9997478b8cc30
a-honey <jungahyeon0512@khu.ac.kr>
2023-10-29 20:57:49 +0900

    Feat: 디바운싱

--- front/src/components/search/Search.SearchList.tsx
+++ front/src/components/search/Search.SearchList.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import styles from './Search.SearchList.module.scss';
+import { ca } from 'date-fns/locale';
 
 const SearchList = ({
   toggleIsOpenSearchList,
@@ -13,21 +14,24 @@ const SearchList = ({
   >(targetName === '유저' ? 'username' : 'title');
   const [searchTerm, setSearchTerm] = useState('');
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-  };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log('검색어 제출', searchTerm);
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchTerm]);
+
   return (
     <div className="modal">
       <div className={styles.container}>
         <h2>{targetName} 검색하기</h2>
-        <form onSubmit={handleSubmit}>
+        <form>
           <input
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <button type="submit" className="doneBtn">
-            검색
-          </button>
         </form>
         <div>
           <SearchItem />
