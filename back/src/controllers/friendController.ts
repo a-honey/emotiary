200ae86e3e98f59a9048ed2a5cc361969494e619
[AI8기/1반] 최은진 <choiej.3190@gmail.com>
2023-11-02 05:09:03 +0900

    Fix: cors 에러 수정중,..

--- back/src/controllers/friendController.ts
+++ back/src/controllers/friendController.ts
@@ -11,11 +11,6 @@ import {
   deleteFriend
 } from '../services/friendService';
 import { IRequest } from "types/user";
-// import { plainToClass } from 'class-transformer';
-// import { FriendValidateDTO } from '../dtos/friendDTO';
-// import { validate } from 'class-validator';
-
-
 
 /** @description 친구 요청 */
 /** @Tag(name = "book service", description = "the book API with description tag annotation") */
@@ -24,7 +19,6 @@ export const friendRequest = async (req : IRequest, res : Response, next : NextF
    * #swagger.tags = ['Friend']
    * #swagger.summary = '친구 요청'
    */
-  try {
     const userId = req.user.id;
     const requestId = req.params.userId;
 
@@ -43,11 +37,6 @@ export const friendRequest = async (req : IRequest, res : Response, next : NextF
     }
     const request = await createFriends(userId, requestId);
     res.status(200).json(request);
-} catch (error) {
-    console.error(error);
-    error.status = 500;
-    next(error);
-  }
 };
 
 /** @description 보낸 친구 요청 목록 */
@@ -56,17 +45,12 @@ export const sentList = async (req : IRequest, res : Response, next : NextFuncti
    * #swagger.tags = ['Friend']
    * #swagger.summary = '보낸 친구 요청 목록'
    */
-  try {
-    const userId = req.user.id;
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 8;
-    const friendRequest = await listRequestsSent(userId, page, limit);
-    res.status(200).json(friendRequest);
-  } catch (error) {
-    console.error(error);
-    error.status = 500;
-    next(error);
-  }
+
+  const userId = req.user.id;
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 8;
+  const friendRequest = await listRequestsSent(userId, page, limit);
+  res.status(200).json(friendRequest);
 };
 
 /** @description 요청 취소 */
@@ -95,17 +79,12 @@ export const receivedList = async (req : IRequest, res : Response, next : NextFu
    * #swagger.tags = ['Friend']
    * #swagger.summary = '받은 친구 요청 목록'
    */
-  try {
-    const userId = req.user.id;
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 8;
-    const friendRequest = await listRequestsReceived(userId, page, limit);
-    res.status(200).json(friendRequest);
-  } catch (error) {
-    console.error(error);
-    error.status = 500;
-    next(error);
-  }
+
+  const userId = req.user.id;
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 8;
+  const friendRequest = await listRequestsReceived(userId, page, limit);
+  res.status(200).json(friendRequest);
 };
 
 /** @description 친구 수락 */
@@ -114,16 +93,11 @@ export const friendAccept = async (req : IRequest, res : Response, next : NextFu
    * #swagger.tags = ['Friend']
    * #swagger.summary = '친구 수락'
    */
-  try {
-    const userId = req.user.id;
-    const requestId = req.params.userId;
-    const accept = await acceptFriend(userId, requestId);
-    res.status(200).json(accept);
-  } catch (error) {
-    console.error(error);
-    error.status = 500;
-    next(error);
-  }
+
+  const userId = req.user.id;
+  const requestId = req.params.userId;
+  const accept = await acceptFriend(userId, requestId);
+  res.status(200).json(accept);
 };
 
 /** @description 친구 거절 */
@@ -132,16 +106,11 @@ export const friendReject = async (req : IRequest, res : Response, next : NextFu
    * #swagger.tags = ['Friend']
    * #swagger.summary = '친구 거절'
    */
-  try {
-    const userId = req.user.id;
-    const requestId = req.params.userId;
-    const reject = await rejectFriend(userId, requestId);
-    res.status(200).json(reject);
-} catch (error) {
-    console.error(error);
-    error.status = 500;
-    next(error);
-  }
+
+  const userId = req.user.id;
+  const requestId = req.params.userId;
+  const reject = await rejectFriend(userId, requestId);
+  res.status(200).json(reject);
 };
 
 /** @description 친구 목록 */
@@ -150,17 +119,12 @@ export const getFriends = async (req : IRequest, res : Response, next : NextFunc
    * #swagger.tags = ['Friend']
    * #swagger.summary = '친구 목록'
    */
-  try {
-    const userId = req.user.id;
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 8;
-    const friendsList = await getMyFriends(userId, page, limit);
-    res.status(200).json(friendsList);
-} catch (error) {
-    console.error(error);
-    error.status = 500;
-    next(error);
-  }
+
+  const userId = req.user.id;
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 8;
+  const friendsList = await getMyFriends(userId, page, limit);
+  res.status(200).json(friendsList);
 };
 
 /** @description 친구 삭제 */
@@ -169,14 +133,9 @@ export const friendDelete = async (req : IRequest, res : Response, next : NextFu
    * #swagger.tags = ['Friend']
    * #swagger.summary = '친구 삭제'
    */
-  try {
-    const userId = req.user.id;
-    const friendId = req.params.userId;
-    const dropFriend = await deleteFriend(userId, friendId);
-    res.status(200).json(dropFriend);
-} catch (error) {
-    console.error(error);
-    error.status = 500;
-    next(error);
-  }
+
+  const userId = req.user.id;
+  const friendId = req.params.userId;
+  const dropFriend = await deleteFriend(userId, friendId);
+  res.status(200).json(dropFriend);
 };
