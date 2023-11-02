85260d748e395633e62cdb53d3555a794bd59d6f
a-honey <jungahyeon0512@khu.ac.kr>
2023-11-02 23:38:52 +0900

    Feat: 인트로페이지 구성

--- front/src/pages/intro/components/Intro.FirstBox.tsx
+++ front/src/pages/intro/components/Intro.FirstBox.tsx
@@ -7,11 +7,7 @@ const FirstBox = () => {
   const { isAnimated, boxRef } = useIsScrollAnimation();
 
   return (
-    <section className={styles.block} ref={boxRef}>
-      <div>
-        <div>환영합니다! 감정 분석 일기 작성 서비스로 여러분을 초대합니다.</div>
-        <div>감정을 기록하고, 우리의 AI가 감정 이모지로 해석해드립니다.</div>
-      </div>
+    <section className={`${styles.first} ${styles.block}`} ref={boxRef}>
       <div
         className={
           isAnimated ? `${styles.animation} ${styles.emojis}` : styles.emojis
@@ -20,6 +16,15 @@ const FirstBox = () => {
         <div className={styles.emoji}>😆</div>
         <div className={styles.emoji}>😮</div>
         <div className={styles.emoji}>😠</div>
+      </div>
+      <div className={styles.textBlock}>
+        <div className={styles.text}>
+          환영합니다! 감정 분석 일기 작성 서비스 <span>Emotiary</span> 로
+          여러분을 초대합니다.
+        </div>
+        <div className={styles.text}>
+          감정을 기록하고, 우리의 AI가 감정 이모지로 해석해드립니다.
+        </div>
       </div>
     </section>
   );
